a42618e2fb56c94c3ecfc1da0e20b7c882596e5f
Lucy Midgley <lucymidgley13@gmail.com>
2019-11-27 22:31:29 +0000

    Create a function to choose school using an if loop

---
+++ whichSchool.js
@@ -0,0 +1,9 @@
+const whichSchool = function(age){
+  if(age<13){console.log("Elementary school")
+    }
+    else if (13 <= age && age <= 18){console.log("Secondary School")
+    }
+    else{console.log("lighthouse labs")
+    }
+}
+whichSchool(25)
